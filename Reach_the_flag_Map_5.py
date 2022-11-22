8c1a9fa2b61bd73fe37ab1dfa6cbb0e6002b213d
Tin Ta <tatrungtin2004@gmail.com>
2022-11-22 10:13:16 +0700

    22/11

--- Reach_the_flag_Map_5.py
+++ Reach_the_flag_Map_5.py
@@ -1,16 +1,16 @@
 import random 
 #Map game 5:
 map5 = [[0,0,0,1,0],[0,0,0,5,1],[0,0,0,1,0],[0,0,0,1,0],[1,1,1,3,1],[0,0,0,1,0]]
-#Vị trí đích:
+#Vi tri dich
 a0 = 4
 b0 = 1 
-#Vị trí ban đầu: 
+#Vi tri ban dau
 a1 = 0
 b1 = 4
 map5[b1][a1] = 0
 
 move_list = ['r','l','u','d']
-
+elite_size = 3
 chromosome_length = 20
 
 population_size = 20
@@ -29,10 +29,11 @@ def fitness(population):
     for chromosome in population:
         a = a1
         b = b1
-        map = [[0,0,0,1,0],[0,0,0,5,1],[0,0,0,1,0],[0,0,0,1,0],[1,1,1,3,1],[0,0,0,1,0]] #Khong tao duoc Copy cua map5
+        map = [[0,0,0,1,0],[0,0,0,5,1],[0,0,0,1,0],[0,0,0,1,0],[1,1,1,3,1],[0,0,0,1,0]] 
         result = []
-        match = 0 #Do phu hop cua tung Chromosome
-        distance = 0
+        match = 0 #Số ô còn lại
+        distance = 0 #Khoảng cách từ vị trí cuối đến vị trí đích
+        #Chạy thử các bước di chuyển trên bản đồ
         for i in chromosome: 
             if i == 'u': 
                 b+= 1
@@ -83,6 +84,7 @@ def fitness(population):
         fitness_scores.append(result)
     return fitness_scores
 
+#Chọn những Choromosome có fitness_score cao nhất để làm thể hệ bố mẹ
 def select_parents(fitness_scores):
     parents_list = []
     for chromosome in sorted(fitness_scores,key = lambda x: x[1])[:parents_number]:
@@ -91,5 +93,19 @@ def select_parents(fitness_scores):
 
 def crossover(parent1,parent2):
     child = []
-    
 
+    #parent1 = parents[0]
+    #parent2 = parents[1]
+    
+    geneA = int(random.random() * chromosome_length)
+    geneB = int(random.random() * chromosome_length)
+
+    startGene = min(geneA, geneB) #điểm bắt đầu và kết thúc crossover
+    endGene = max(geneA, geneB)
+
+    for i in range(0,chromosome_length):
+        if (i < startGene) or (i > endGene):
+            child.append(parent1[i])
+        else:
+            child.append(parent2[i])
+    return child
